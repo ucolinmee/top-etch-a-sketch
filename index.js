d090ff0afee6823806da73126eb9ed75f2f77ccb
ucolinmee <colinjmeehan23@gmail.com>
2024-02-09 00:28:41 +0800

    Created static 16x16 grid with fixed sized boxes

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-alert("This works");
+var container = document.querySelector(".container");
+for (var i=0; i<256; i++) {
+    var div = document.createElement("div");
+    div.classList.add("grid");
+    container.appendChild(div);
+}
+
+// TODO: Add a input field to take user's specified dimensions
+// TODO: Find out how to dynamically set the width (and height) of the grid boxes based on user's specified dimensions
+// TODO: Add hover effect event listener 
