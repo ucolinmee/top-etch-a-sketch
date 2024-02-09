5455c25d65dc80dd163d7680ee1a3b72804266bd
ucolinmee <colinjmeehan23@gmail.com>
2024-02-09 23:34:27 +0800

    Add hover effect functionality

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Create grid layout
 var container = document.querySelector(".container");
 for (var i=0; i<256; i++) {
     var div = document.createElement("div");
@@ -5,6 +6,16 @@ for (var i=0; i<256; i++) {
     container.appendChild(div);
 }
 
+// Hover effect event listener
+var grids = document.querySelectorAll(".grid");
+grids.forEach(grid => {
+    grid.addEventListener("mouseover", (e) => {
+        e.target.classList.add("hover");
+    })
+    grid.addEventListener("mouseout", (e) => {
+        e.target.classList.remove("hover");
+    })
+});
+
 // TODO: Add a input field to take user's specified dimensions
 // TODO: Find out how to dynamically set the width (and height) of the grid boxes based on user's specified dimensions
-// TODO: Add hover effect event listener 
